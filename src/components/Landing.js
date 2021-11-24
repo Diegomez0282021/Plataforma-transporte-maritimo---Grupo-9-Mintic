684786bb4face7ed0a58b7aa5c5964ed7ef12d88
Osc2405 <orosero2405@gmail.com>
2021-11-24 17:36:16 -0500

    Primer commit

---
+++ src/components/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Landing() {
+    return (
+        <section className="highlight-phone">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8">
+                    <div className="intro">
+                        <h2>¡Bienvenidos!</h2>
+                        <p className="text-start">Nuestra empresa se encarga de transportar mercancías por vía fluvial. Contamos con un buen numero de embarcaciones y transportamos tus contenedores al puerto que mas te convenga.<br/>Regístrate para poder disfrutar de todos nuestros servicios.</p><a className="btn btn-primary" role="button" href="register.html">Registrarse</a>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="d-none d-md-block phone-mockup"><img src="assets/img/kisspng-watercraft-cargo-ship-a-ship-laden-with-cargo-5a9957609ee080.7249956015199988166508%20(1).png" width="300" alt="Imagen-Barco"/></div>
+                </div>
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-light navbar-expand-md navigation-clean-button">
+        <div className="container"><img src="assets/img/logo.png" width="70" alt="Logo"/><a className="navbar-brand" href="/">Compañia Transnaval S.A.S.</a><button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1"><span className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+            <div className="collapse navbar-collapse" id="navcol-1">
+                <ul className="navbar-nav me-auto">
+                    <li className="nav-item"><a className="nav-link" href="generar-orden.html">Generar Orden</a></li>
+                    <li className="nav-item"><a className="nav-link" href="historial.html">Historial</a></li>
+                    <li className="nav-item"><a className="nav-link" href="consultar.html">Consultar</a></li>
+                    <li className="nav-item"></li>
+                    <li className="nav-item dropdown"><a className="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="/">Dashboard</a>
+                        <div className="dropdown-menu"><a className="dropdown-item" href="ingresar-puerto.html">Ingresar puerto</a><a className="dropdown-item" href="configurar-valor-milla.html">Configurar valor milla nautica</a><a className="dropdown-item" href="aceptar-orden.html">Aceptar orden</a><a className="dropdown-item" href="cambiar-estado.html">Cambiar estado de orden</a><a className="dropdown-item" href="ordenes-dia.html">Ordenes del dia</a></div>
+                    </li>
+                    </ul><span className="navbar-text actions"> <a className="login" href="login.html">Log In</a><a className="btn btn-light action-button" role="button" href="register.html">Sign Up</a></span>
+                </div>
+            </div>
+        </nav>
+    )
+}
