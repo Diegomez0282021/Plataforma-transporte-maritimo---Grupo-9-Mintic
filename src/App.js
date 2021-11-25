63fbbd346ca2aba9f5a34bf47dd92c24e8175942
Diegomez0282021 <diegomez028@gmail.com>
2021-11-25 10:21:27 -0500

    Agregando enrutamiento a cambiar-estado, aceptar-orden y ordenes-dia

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React,{Fragment} from 'react';
 import './App.css';
-import React from 'react';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import Footer from './components/Footer';
@@ -8,18 +7,12 @@ import Login from './components/Login';
 import Register from './components/Register';
 import ConfigurarValorMilla from './components/ConfigurarValorMilla';
 import OrdenesDia from './components/ordenes-del-dia';
+import AceptarOrden from './components/aceptar-orden';
+import CambiaEstado from './components/cambiar-estado-orden';
 import {BrowserRouter as Router, Routes, Route,Link} from 'react-router-dom'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      <Navbar />
-      <Landing />
-      <Footer />  
-   
-    </div>
-=======
     <Router>
       <Fragment>
         <Navbar />
@@ -29,12 +22,13 @@ function App() {
         <Route path='/register' element={<Register/>} />
         <Route path='/configurar-valor-milla' element={<ConfigurarValorMilla/>} />
         <Route path='/ordenes-dia' element={<OrdenesDia/>} />
+        <Route path='/aceptar-orden' element={<AceptarOrden/>}/>
+        <Route path='/cambiar-estado-orden' element={<CambiaEstado/>}/>
         </Routes>
         <Footer />
         
       </Fragment>
     </Router>
->>>>>>> main
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,7 @@
 import React from 'react'
-<<<<<<< HEAD
-=======
 import {Link} from 'react-router-dom'
 
 
->>>>>>> main
 export default function Navbar() {
     return (
         <nav className="navbar navbar-light navbar-expand-md navigation-clean-button">
@@ -15,13 +12,8 @@ export default function Navbar() {
                     <li className="nav-item"><Link className="nav-link" to="/historial">Historial</Link></li>
                     <li className="nav-item"><Link className="nav-link" to="/consultar">Consultar</Link></li>
                     <li className="nav-item"></li>
-<<<<<<< HEAD
-                    <li className="nav-item dropdown"><a className="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="/">Dashboard</a>
-                        <div className="dropdown-menu"><a className="dropdown-item" href="ingresar-puerto.html">Ingresar puerto</a><a className="dropdown-item" href="configurar-valor-milla.html">Configurar valor milla nautica</a><a className="dropdown-item" href="aceptar-orden.html">Aceptar orden</a><a className="dropdown-item" href="cambiar-estado.html">Cambiar estado de orden</a><a className="dropdown-item" href="ordenes-dia.html">Ordenes del dia</a></div>                       
-=======
                     <li className="nav-item dropdown"><Link className="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" to="/">Dashboard</Link>
-                        <div className="dropdown-menu"><Link className="dropdown-item" to="/ingresar-puerto">Ingresar puerto</Link><Link className="dropdown-item" to="/configurar-valor-milla">Configurar valor milla nautica</Link><Link className="dropdown-item" to="/aceptar-orden">Aceptar orden</Link><Link className="dropdown-item" to="/cambiar-estado">Cambiar estado de orden</Link><Link className="dropdown-item" to="/ordenes-dia">Ordenes del dia</Link></div>
->>>>>>> main
+                        <div className="dropdown-menu"><Link className="dropdown-item" to="/ingresar-puerto">Ingresar puerto</Link><Link className="dropdown-item" to="/configurar-valor-milla">Configurar valor milla nautica</Link><Link className="dropdown-item" to="/aceptar-orden">Aceptar orden</Link><Link className="dropdown-item" to="/cambiar-estado-orden">Cambiar estado de orden</Link><Link className="dropdown-item" to="/ordenes-dia">Ordenes del dia</Link></div>
                     </li>
                     </ul><span className="navbar-text actions"> <Link className="login" to="/login">Log In</Link><Link className="btn btn-light action-button" role="button" to="/register">Sign Up</Link></span>
                 </div>
