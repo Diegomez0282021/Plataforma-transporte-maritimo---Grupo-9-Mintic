65e3e9826ee9bd48b353cbf21497248ff0ccad22
Diegomez0282021 <diegomez028@gmail.com>
2021-11-24 23:20:22 -0500

    Agregando ordenes-del-dia.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import Footer from './components/Footer';
-
+import React  from 'react';
 function App() {
   return (
     <div>
