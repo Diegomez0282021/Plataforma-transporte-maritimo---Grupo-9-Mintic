38860635bd73b4cb6c99a7f2a6affc80eebe27ca
Osc2405 <orosero2405@gmail.com>
2021-12-19 16:17:34 -0500

    Aceptar Orden terminado

--- src/services/orden.services.js
+++ src/services/orden.services.js
@@ -18,4 +18,23 @@ const saveOrders = (data) => {
   });
 };
 
-export { getOrders, saveOrders };
+const updateOrders = (data) => {
+  const headers = {
+    Authorization: localStorage.token,
+    "Content-Type": "application/json; charset=utf-8",
+  };
+  return axios.post(`http://localhost:3001/api/order/update`, JSON.stringify(data), {
+    headers,
+  });
+};
+
+const deleteOrders = (dato) => {
+  const headers = {
+    Authorization: localStorage.token,
+    "Content-Type": "application/json; charset=utf-8",
+  };
+  return axios.delete(`http://localhost:3001/api/order/remove`,{ data:dato});
+};
+
+
+export { getOrders, saveOrders,updateOrders,deleteOrders };
