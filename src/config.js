b330de1bd523cb6ac17306027982889ea1c89fd1
Diegomez0282021 <91164743+Diegomez0282021@users.noreply.github.com>
2021-12-20 19:46:58 -0500

    Update config.js
    cambio para deploy heroku

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = global.config = {
-    URL: "http://localhost:3000/",
-    URLAPI: "http://localhost:3001"
+    URL: "https://minticbackentrans.herokuapp.com/",
+    URLAPI: "https://minticbackentrans.herokuapp.com/"
 };
